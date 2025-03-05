027ab27b2dbd3a03b3379753cb5c53ef37ebea8d
nayan911 <nayanagarwal911@gmail.com>
2025-03-05 23:15:11 +0530

    created functions

---
+++ index.js
@@ -0,0 +1,23 @@
+import { eq, ilike } from 'drizzle-orm';
+import { db } from './db';
+import { todosTable } from './db/schema';
+
+async function getAllTodos() {
+  const todos = await db.select().from(todosTable);
+  return todos;
+}
+
+async function createTodo(todo) {
+    await db.insert(todosTable).values({
+        todo,
+    });
+}
+
+async function deleteTodoById(id) {
+    await db.delete(todosTable).where(eq(todosTable.id, id));
+}
+
+async function searchTodo(search) {
+    const todos = await db.select().from(todosTable).where(ilike(todosTable.todo, search));
+    return todos;
+}
